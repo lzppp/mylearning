bf00a8bc1bfd3bf64a74790a40cb02b18d6792f9
lzppp <lzppp1114@gmail.com>
2017-01-16 18:04:32 +0800

    sa and shortest path 0.46 test sa

--- ryu/app/network_awareness/shortest_forwarding.py
+++ ryu/app/network_awareness/shortest_forwarding.py
@@ -428,6 +428,7 @@ class ShortestForwarding(app_manager.RyuApp):
             self.flow_infome[(ip_src , ip_dst)]['datapath']=datapath
             graph = copy.deepcopy(self.awareness.graph)
             flow_in_road = copy.deepcopy(self.monitor.flow_in_road)
+            '''
             for ip in self.vip:
                 result = self.get_sw(self.flow_infome[(ip_src , ip)]['datapath'].id, self.flow_infome[(ip_src , ip)]['in_port'], ip_src, ip)
                 src_sw, dst_sw = result[0] , result[1]
@@ -443,6 +444,7 @@ class ShortestForwarding(app_manager.RyuApp):
                                         flow_info, msg.buffer_id, msg.data)
                     graphchange(graph , path)
                     del flow_in_road[(ip_src , ip)]
+            '''
             for key in flow_in_road.keys():
                 flow_in_road[key]['src'] = self.flow_infome[key]['src']
                 flow_in_road[key]['dst'] = self.flow_infome[key]['dst']
