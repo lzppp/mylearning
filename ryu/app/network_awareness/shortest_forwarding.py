9c1aecb4d568a6418362a9cd92799cdf56cabbe4
lzppp <lzppp1114@gmail.com>
2017-01-17 10:44:33 +0800

    only sa while src= 202.116

--- ryu/app/network_awareness/shortest_forwarding.py
+++ ryu/app/network_awareness/shortest_forwarding.py
@@ -419,7 +419,7 @@ class ShortestForwarding(app_manager.RyuApp):
         """
             todo!!!!! 
         """
-        if ip_src == '202.116.7.106' or ip_dst == '202.116.7.106':
+        if ip_src == '202.116.7.106':
             '''
                 do QoE APP AWARE
             '''
